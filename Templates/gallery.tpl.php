a1291c738a6f6faf3e322d6a6252a91dee028673
DAB <dab@dab.la>
2019-08-03 08:29:33 -0500

    fixed captions

--- Templates/gallery.tpl.php
+++ Templates/gallery.tpl.php
@@ -9,8 +9,8 @@ use \Modules\ImageManager\Model\Image;
             <ul class="small-block-grid-2 medium-block-grid-4 large-block-grid-5">
                 <?php foreach ($gallery->getImages() as $image): ?>
                 <li class="gallery-item">
-                    <figure class="gallery-image cover" style="margin:0;height:100%;background-image:url(<?=Image::getImage($image['image'], 250, \Lightning\Tools\Image::FORMAT_JPG);?>" style="padding-top:10px;" alt="<?=$image['description'];?>)">
-                        <div data-href="<?=Image::getImage($image['image'], 1000, \Lightning\Tools\Image::FORMAT_JPG);?>" itemprop="contentUrl" class="show-link" data-index="<?=$index++;?>" style="min-height:200px;">
+                    <figure class="gallery-image" style="margin:0;padding-top:10px;" alt="<?=$image['description'];?>)">
+                        <div data-href="<?=Image::getImage($image['image'], 1000, \Lightning\Tools\Image::FORMAT_JPG);?>" itemprop="contentUrl" class="show-link cover" data-index="<?=$index++;?>" style="background-image:url(<?=Image::getImage($image['image'], 250, \Lightning\Tools\Image::FORMAT_JPG);?>);min-height:200px;">
                         </div>
                         <figcaption>
                             <?php if (!empty($image['title'])): ?>
